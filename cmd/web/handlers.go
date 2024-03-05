b5ba1c9758e2df83b1ba985e100aa14fa62f93d0
m4rk1sov <134860120+m4rk1sov@users.noreply.github.com>
2024-03-05 21:08:25 +0500

    Starting of the development
    Copying from the assignments

---
+++ cmd/web/handlers.go
@@ -0,0 +1,278 @@
+package main
+
+import (
+	"almasmagzumov.net/snippetbox/pkg/forms"
+	"almasmagzumov.net/snippetbox/pkg/models"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
+}
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", &templateData{
+		// Pass a new empty forms.Form object to the template.
+		Form: forms.New(nil),
+	})
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
+		return
+	}
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	// Use the Put() method to add a string value ("Your snippet was saved
+	// successfully!") and the corresponding key ("flash") to the session
+	// data. Note that if there's no existing session for the current user
+	// (or their session has expired) then a new, empty, session for them
+	// will automatically be created by the session middleware.
+	app.session.Put(r, "flash", "Article successfully created!")
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+}
+
+func (app *application) students(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/students" {
+		app.notFound(w)
+		return
+	}
+	s, err := app.snippets.Students()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.render(w, r, "students.page.tmpl", &templateData{
+		Snippets: s,
+	})
+}
+
+func (app *application) createDepartmentForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.department.page.tmpl", &templateData{
+		// Pass a new empty forms.Form object to the template.
+		Form: forms.New(nil),
+	})
+}
+
+func (app *application) createDepartment(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	form := forms.New(r.PostForm)
+	form.Required("dep_name", "staff_quantity")
+	//form.MaxLength("title", 100)
+
+	if !form.Valid() {
+		app.render(w, r, "create.department.page.tmpl", &templateData{Form: form})
+		return
+	}
+	id, err := app.snippets.InsertDepartments(form.Get("dep_name"), form.Get("staff_quantity"))
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.session.Put(r, "flash", "Department successfully created!")
+	http.Redirect(w, r, fmt.Sprintf("/department/%d", id), http.StatusSeeOther)
+}
+
+func (app *application) Departments(w http.ResponseWriter, r *http.Request) {
+	s, err := app.snippets.ShowDepartments()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.render(w, r, "departments.page.tmpl", &templateData{
+		Snippets: s,
+	})
+}
+
+func (app *application) showDepartment(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	s, err := app.snippets.GetDepartment(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	app.render(w, r, "show.department.page.tmpl", &templateData{
+		Snippet: s,
+	})
+}
+
+func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "signup.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
+	// Parse the form data.
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// Validate the form contents using the form helper we made earlier.
+	form := forms.New(r.PostForm)
+	form.Required("name", "email", "password")
+	form.MaxLength("name", 255)
+	form.MaxLength("email", 255)
+	form.MatchesPattern("email", forms.EmailRX)
+	form.MinLength("password", 10)
+	// If there are any errors, redisplay the signup form.
+	if !form.Valid() {
+		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		return
+	}
+	// Try to create a new user record in the database. If the email already exists
+	// add an error message to the form and re-display it.
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Address is already in use")
+			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in.
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+
+}
+func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "login.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// Check whether the credentials are valid. If they're not, add a generic error
+	// message to the form failures map and re-display the login page.
+	form := forms.New(r.PostForm)
+	id, err := app.users.Authenticate(form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.Errors.Add("generic", "Email or Password is incorrect")
+			app.render(w, r, "login.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	// Add the ID of the current user to the session, so that they are now 'logged
+	// in'.
+	app.session.Put(r, "authenticatedUserID", id)
+	// Redirect the user to the create snippet page.
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
+	// Remove the authenticatedUserID from the session data so that the user is
+	// 'logged out'.
+	app.session.Remove(r, "authenticatedUserID")
+	// Add a flash message to the session to confirm to the user that they've been
+	// logged out.
+	app.session.Put(r, "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (app *application) adminPanelForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "admin.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (app *application) adminPanel(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("email")
+	form.MatchesPattern("email", forms.EmailRX)
+
+	if !form.Valid() {
+		app.render(w, r, "admin.page.tmpl", &templateData{Form: form})
+		return
+	}
+
+	err = app.users.SetTeacher(form.Get("email"))
+
+	if err != nil {
+		if errors.Is(err, models.ErrEmailDoesNotExist) {
+			form.Errors.Add("email", "No user with such email")
+			app.render(w, r, "admin.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.session.Put(r, "flash", "Role was set successfully")
+
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	// Create a new middleware chain containing the middleware specific to
+	// our dynamic application routes. For now, this chain will only contain
+	// the session middleware but we'll add more to it later.
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf, app.authenticate)
+	mux := pat.New()
+	// Update these routes to use the new dynamic middleware chain followed
+	// by the appropriate handler function.
+
+	// Routes
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+
+	mux.Get("/snippet/create", dynamicMiddleware.Append(app.requireRoleTeacher).ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.Append(app.requireRoleTeacher).ThenFunc(app.createSnippet))
+	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
+
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+
+	mux.Get("/department/create", dynamicMiddleware.Append(app.requireRoleTeacher).ThenFunc(app.createDepartmentForm))
+	mux.Post("/department/create", dynamicMiddleware.Append(app.requireRoleTeacher).ThenFunc(app.createDepartment))
+
+	mux.Get("/students", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.students))
+	mux.Get("/department", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.Departments))
+	mux.Get("/department/:id", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.showDepartment))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
+
+	// Routes for admin only
+	mux.Get("/admin", dynamicMiddleware.Append(app.requireRoleAdmin).ThenFunc(app.adminPanelForm))
+	mux.Post("/admin", dynamicMiddleware.Append(app.requireRoleAdmin).ThenFunc(app.adminPanel))
+
+	// Leave the static files route unchanged.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+	return standardMiddleware.Then(mux)
+}
